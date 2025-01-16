5b4d42989739db54aca0a90380a9b9f17ba28a13
taskcat1234 <1232168@naver.com>
2025-01-16 17:45:26 +0900

    2025-01-16(최종) 24 수정 실습 06 완료

--- js/24_추가JS기능.js
+++ js/24_추가JS기능.js
@@ -45,3 +45,111 @@ btn1.addEventListener("click", ()=>{
   // -> 해결 방법 : 문자열로 인식할 수 있게 "",'' 감싸기
   // (참고 : JS객체의 Key는 모두 string 자료형이다)
 });
+
+
+/* 구조 분해 할당 확인 */
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click",()=>{
+  //[배열 + 구조 분해 할당]
+  const fruits = ['사과', '바나나', '오렌지'];
+    
+  // const first = fruits[0]; // '사과'
+  // const second = fruits[1]; // '바나나'
+  // const thrid = fruits[2]; // '오렌지'
+
+  // 위에 변수 선언/대입 구문을 구조분해 할당으로 변경
+  const [first, second, third] = fruits;
+  console.log(first, second, third)
+
+  //[객체 + 구조 분해 할당]
+  const person = {name : "홍길동", age : 25, height : 180};
+  // 기존 방법
+  // const name   = person.name;
+  // const age    = person.age;
+  // const height = person.height;
+  const {name, age, height} = person;
+  console.log(name, age, height);
+});
+
+
+//----------------------------------------------------------------------------------
+/* 배열 + ... 연산자 */
+const btn3 = document.querySelector("#btn3");
+btn3.addEventListener("click", () =>{
+
+  const arr = [10,20,30];
+  const copy1 = arr;
+  console.log("arr : ", arr);
+  console.log("copy : ", arr);
+
+  copy1[0] = 9999; //복사본에 0번 이덱스에 999 대입
+  console.log("copy1[0] = 999");
+  console.log("arr :", arr);
+  console.log("copy1 : ", copy1);
+
+  // 왜 arr[0]이 999로 변했을까?
+  // -> 얕은 복사가 되었기 때문이다!!!!
+  
+  // * 얕은 복사(Shallow Copy)
+  // - 같은 주소를 복사하여 저장한
+  //   여러 변수가 하나의 객체를 참조하는것
+
+  // ------------------------------------------------------------------------------
+
+  // * 깊은 복사(Deep Copy)
+  // - 객체의 모양과 저장된 값을 그대로 복사 == 복제
+
+  // **전개 연산자 사용!!**
+  const copy2 = [...arr]; // [999,20,30]
+  // -> arr이 참조하는 값을 얻어와 새로운 배열로 생성
+  copy2[1] = 1234;
+  console.log("copy2[1] = 1234");
+  console.log("arr :", arr);       // [999,20,30]
+  console.log("copy2 : ", copy2);  // [999,1234,30]
+
+  // ---------------------------------------------------
+
+  /* 배열 결합 확인 */
+  const students1 = ["홍길동","둘리","또치"];
+  const students2 = ["짱구","유리", "맹구"];
+  const students3 = [...students1,...students2];
+  //const students4 = ["홍길동","둘리","또치","짱구","유리", "맹구"]
+  console.log("students3 : ",students3)
+
+  // ---------------------------------------------------------------
+  /* 배열 요소 추가 확인 */
+  const students4 = [...students3, "철수", "희동이"];
+  console.log("students4 : ", students4)
+});
+
+
+/* 객체 + ... 연산자 */
+const btn4 = document.querySelector("#btn4");
+btn4.addEventListener("click",()=>{
+
+  /* 객체 복사 */
+  const obj = {id : "user01", pw : "pass01"}
+  const copy1 = obj;     // 얕은 복사 (copy1은 obj와 같은 객체 참조)
+  const copy2 = {...obj} // 깊은 복사 (copy2는 obj 복제본 참조)
+
+  copy1.pw = "1q2w3e4r";
+  console.log("obj : ", obj)
+  console.log("copy1 : ", copy1)
+  console.log("copy2 : ", copy2)
+
+  /* 객체 병합 */
+  const obj2 = {id:"user02", pw:"pass02"};
+  const obj3 = {email:"user02@gmail.com", phone:"01012341234"};
+  const obj4 = {...obj2, ...obj3};
+  console.log("obj4 : ",obj4);
+
+
+  /* 속성 덮어 쓰기 */
+  const obj5 = {...obj4, phone:"01099998888"};
+  console.log("obj5 : ",obj5);
+
+
+
+
+
+});
--- 연습문제/옵션적용하기/js/06_실습문제.js
+++ 연습문제/옵션적용하기/js/06_실습문제.js
@@ -1,15 +1,217 @@
-document.getElementById('updateBtn').addEventListener('click', function() {
-  // 수정 버튼 위의 div에 b-hidden 추가
-  document.querySelector('.normal-container').classList.add('b-hidden');
+const resName = document.querySelector("#resName");
+const resNameInput = document.querySelector("#resNameInput");
+
+// 버튼 4개
+const updateBtn  = document.querySelector("#updateBtn");
+const addMenu    = document.querySelector("#addMenu");
+const deleteMenu = document.querySelector("#deleteMenu");
+const exitBtn    = document.querySelector("#exitBtn");
+
+// 버튼 영역
+const normalContainer = document.querySelector(".normal-container");
+const editContainer = document.querySelector(".edit-container");
+
+// 메뉴판
+const menuContainer = document.querySelector(".menu-container");
+
+
+
+
+/* 식당명 관련 기능 구현*/
+
+
+/* #resName (span) 이 클릭된 경우
+  #resName을 숨기고
+  #resNameInput을 보이게 만들기*/
+resName.addEventListener("click", () => {
+
+  // span 숨기기 클래스 추가
+  resName.classList.add("res-name-hidden"); 
   
-  // 밑에 div에서 b-hidden 클래스 제거
-  document.querySelector('.edit-container').classList.remove('b-hidden');
+  // input 숨기기 클래스 제거
+  resNameInput.classList.remove("res-name-hidden"); 
+
+  resNameInput.focus(); // input에 포커스 맞추기
 });
 
-document.getElementById('exitBtn').addEventListener('click', function() {
-  // 수정 버튼 위의 div에서 b-hidden 클래스를 제거
-  document.querySelector('.normal-container').classList.remove('b-hidden');
-  
-  // 밑에 div에 b-hidden 클래스를 다시 추가
-  document.querySelector('.edit-container').classList.add('b-hidden');
+
+
+/* #resNameInput이 포커스를 잃었을 때(blur) 
+  #resNameInput을 숨기고
+  #resName을 보이게 만들기*/
+resNameInput.addEventListener("blur", () => {
+  resNameInput.classList.add("res-name-hidden");
+  resName.classList.remove("res-name-hidden");
+
+  // input에 입력된 값이 없다면
+  if(resNameInput.value.trim() === ""){
+    resName.innerText = "식당명을 입력하세요";
+    return;
+  }
+
+  // input에 작성된 내용을 span에 그대로 대입
+  resName.innerText = resNameInput.value;
 });
+
+
+// -----------------------------------------------
+/* 수정 버튼 클릭 시 */
+updateBtn.addEventListener("click", () => {
+
+  // 1) 버튼 영역 변경
+  normalContainer.classList.toggle("b-hidden");
+  editContainer.classList.toggle("b-hidden");
+
+  // 2) .menu 모두 얻어오기
+  const menus = document.querySelectorAll(".menu");
+
+  // 3) 모든 .menu를 순차 접근
+  menus.forEach(item => {
+
+    // 4) 기존 메뉴판에 작성된 메뉴명, 가격 얻어오기
+    const menuName = item.children[0].innerText;
+    const menuPrice = item.children[1].innerText.replace('원','');
+    
+    // 5) .menu 내부에 추가될 input요소를 모아둔 JS객체 얻어오기
+    const menuContent = createMenuContent();
+
+    //삼항 연산자
+    // -> 조건식 ? true인 경우 : false인 경우;
+    // -> 조건식 결과에 따라 둘 중 하나를 선택!
+
+    menuContent.menuNameInput.value = (menuName === "미입력") ? "" : menuName;
+
+    menuContent.menuPriceInput.value = (menuPrice === "0") ? "" : menuPrice;
+
+    // .menu(li) 내부 내용 모두 삭제
+    item.innerHTML = "";
+
+    // menuContent 객체에 저장된 input 요소를
+    // 모두 .menu(li) 의 자식으로 추가
+    for(let key in menuContent){
+      item.append(menuContent[key]);
+    }
+
+  });
+
+
+});
+
+// -----------------------------------------------
+/* 종료 버튼 클릭 시 */
+exitBtn.addEventListener("click", () => {
+  // 1) 버튼 영역 변경
+  normalContainer.classList.toggle("b-hidden");
+  editContainer.classList.toggle("b-hidden");
+  // 2) .menu 모두 얻어오기
+  const menus = document.querySelectorAll(".menu");
+  // 3) 모든 .menu를 순차 접근
+  menus.forEach(item => {
+    // 4) input에 작성된 메뉴명, 가격 얻어오기
+    const menuName = item.children[1].value;
+    const menuPrice = item.children[2].value;
+  
+    // 5 span 요소 생성
+    const span1 = newEl("span", {},["menu-name"])
+    span1.innerText = (menuName.trim() === "" ? "미입력" : menuName);
+    const span2 = newEl("span", {},["menu-price"])
+    span2.innerText = (menuPrice.trim() === "" ? "미입력" : menuPrice);
+
+    // .menu(li) 내부 내용 모두 삭제
+    item.innerHTML = "";
+
+    // span 모두 .menu(li) 자식으로 추가
+    item.append(span1, span2);
+
+  });
+  
+  // <span class="menu-name">카레 돈까스</span>
+  // <span class="menu-price">10000원</span>
+
+}); 
+// ------------------------------------------------------
+/* 추가 버튼 클릭시에 동작 
+  메뉴에 입력 칸 추가 (최대 15개)
+*/
+addMenu.addEventListener("click", ()=>{
+  // 메뉴가 15개 이상인지 확인
+  if(document.querySelectorAll(".menu").length >= 15){
+    alert("더 이상 메뉴를 추가할 수 없습니다.");
+    return;
+  }
+
+
+  // li 요소 생성 (+ class명 : menu)
+  const li = newEl("li", {}, ["menu"]);
+
+  // li에 들어갈 input 3개 만들어서 객체로 반환 받기
+  const menuContent = createMenuContent();
+
+  // li에 menuContent 값을 모두 추가
+  for(let key in menuContent){
+    li.append(menuContent[key]);
+  }
+
+  // li를 menuContainer(메뉴판)에 추가
+  menuContainer.append(li);
+});
+// ------------------------------------------------------
+/* 삭제 버튼 클릭 시
+  체크된 메뉴만 모두 remove() */
+deleteMenu.addEventListener("click",()=>{
+
+  // .menu-check (체크박스) 중 체크된 요소만 얻어오기
+  const menus = document.querySelectorAll(".menu-check:checked");
+
+  // 체크된 요소 순차 접근
+  menus.forEach(item => {
+
+    // item(체크박스)의 부모(.menu)요소를 제거
+    item.parentElement.remove();
+  });
+});
+
+// ------------------------------------------------------
+
+/* 요소 생성 + 속성 추가 + 클래스 추가 함수 */
+function newEl(tag, attr, cls){
+  const el = document.createElement(tag);
+
+  // attr 매개 변수는 객체 형태
+  for(let key in attr){
+    el.setAttribute(key, attr[key]); // 속성 추가
+  }
+
+  // cls 매개 변수는 배열 형태
+  for(let className of cls){ // 클래스 추가
+    el.classList.add(className);
+  }
+  
+  return el;
+}
+
+
+/* 메뉴 내부 체크박스 요소를 만들어 객체로 반환하는 함수 */
+function createMenuContent(){
+
+  // 체크박스 생성
+  const check = newEl("input", {type:"checkbox"}, ["menu-check"]);
+
+  // 메뉴명 input 생성
+  const menuNameInput 
+    = newEl("input", 
+      {type:"text", placeholder:"메뉴명"}, 
+      ["menu-name-input"]);
+
+  // 메뉴 가격 input 생성
+  const menuPriceInput
+    = newEl("input", 
+      {type:"text", placeholder:"가격"}, 
+      ["menu-price-input"]);
+
+  return {
+    "check" : check,
+    "menuNameInput" : menuNameInput,
+    "menuPriceInput" : menuPriceInput
+  }
+}
